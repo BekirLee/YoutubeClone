568fd0b0c601f35cd8ef8761a10dc890ae026734
BekirLee <nihadbekirli8@gmail.com>
2024-11-12 00:00:08 +0400

    going for new journey youtube wait for me

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './assets/css/tailwind.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div className="underline">hello</div>
+)
---
+++ src/pages/notFound/index.jsx
@@ -0,0 +1,5 @@
+export default function NotFound() {
+    return (
+        <div className="bg-red text-black text-center p-5">Error 404: Page Not Found</div>
+    )
+}
