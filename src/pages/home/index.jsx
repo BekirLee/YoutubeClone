f52e53c42d52d066472b11d164bb3a312ab44f87
BekirLee <nihadbekirli8@gmail.com>
2024-12-07 01:18:17 +0300

    done guide side

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,7 +3,7 @@ import Header from "../../components/Header";
 
 export default function Home() {
     return (
-        <div className="bg-[#0f0f0f] h-screen">
+        <div className="bg-[#0f0f0f]  ">
             {/* Home */}
             <Header />
             <Guide />
