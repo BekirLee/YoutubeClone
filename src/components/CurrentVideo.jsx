19c931dc3b3310905ebd129142ccdffde0e9b3c2
BekirLee <nihadbekirli8@gmail.com>
2025-01-31 01:40:34 +0300

    making structure of rest of currentVideo component

--- src/components/CurrentVideo.jsx
+++ src/components/CurrentVideo.jsx
@@ -12,6 +12,7 @@ function CurrentVideo() {
 
     const [videoData, setVideoData] = useState(null);
     const [channelId, setChannelId] = useState(null);
+    const [channelSub, setSubs] = useState(null);
     const [channelInfos, setChannelInfos] = useState(null);
     const [searchParams] = useSearchParams();
     const videoId = searchParams.get('id'); // Query parametresinden videoId'yi al
@@ -27,12 +28,36 @@ function CurrentVideo() {
         setChannelInfos(res.data.items[0])
         console.log(res.data.items[0])
     }
+    const channelSubs = async () => {
+        const res = await axios.get(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${apikey}`)
+        setSubs(res.data.items[0].statistics.subscriberCount)
+        // console.log(res.data.items[0].statistics.subscriberCount)
+    }
     useEffect(
         () => {
             currentVideoFetch();
-            channelAvatar();
         }, [])
+
+    useEffect(() => {
+
+        channelAvatar();
+        channelSubs();
+    }, [channelId])
     // console.log(videoItems)
+
+    const formatViewCount = (number) => {
+        if (number >= 1_000_000_000) {
+            return (number / 1_000_000_000).toFixed(1) + "B";
+        }
+        if (number >= 1_000_000) {
+            return (number / 1_000_000).toFixed(1) + "M";
+        }
+        if (number >= 1_000) {
+            return (number / 1_000).toFixed(1) + "K";
+        }
+        return number.toString();
+    };
+
     return (
         <div className='p-24 pr-8 pl-4'>
 
@@ -49,19 +74,53 @@ function CurrentVideo() {
                     allowFullScreen
                 ></iframe >
             </div>
+            <div className=" text-white leading-8 mt-3 mb-3">
+                <h3 className='font-bold text-[21px]'>{videoData?.snippet?.title}</h3>
 
-            <div className="text-[21px] text-white font-bold leading-8 mt-3 mb-3">
-                <h3>{videoData ?
-                    videoData.snippet.title : ''}
-                </h3>
-                <h3>
-                    {videoData ? videoData.snippet.title : ''}
-                </h3>
-                <h3>{channelInfos.snippet.title}</h3>
-                {/* <img src={channelInfos.snippet.thumbnails.medium.url} alt="" /> */
-                    channelInfos ?
-                        console.log(channelInfos) : ''
-                }
+                <div className="flex gap-2 items-center h-14">
+
+                    <div className="w-11 h-11 object-cover">
+                        {channelInfos?.snippet?.thumbnails?.medium?.url && (
+                            <img src={channelInfos.snippet.thumbnails.medium.url} alt="Channel Avatar" className='rounded-full' />
+                        )}
+                    </div>
+
+                    <div className=''>
+                        <div className="text-[16px] h-6 font-bold">
+                            {channelInfos?.snippet?.title.length > 5 ? channelInfos?.snippet?.title.slice(0, 5) + '...' : channelInfos?.snippet?.title}
+                        </div>
+                        <div className='text-[13px] h-6 text-[#aaa]'>
+                            {formatViewCount(channelSub)}
+                        </div>
+                    </div>
+
+                    <div className="btn">
+                        <button>
+                            Join
+                        </button>
+                    </div>
+
+                    <div className="btn">
+                        <button>
+                            Subscribe
+                        </button>
+                    </div>
+
+                    <div className="btnLike">
+                        <button className='like'></button>
+                        <button className='dislike'></button>
+                    </div>
+
+                    <div className="share">
+                        <button></button>
+                    </div>
+
+                    <div className="bars"></div>
+                </div>
+
+                <div className="descriptionAndOthers"></div>
+
+                <div className="comments"></div>
             </div>
         </div>
     )
