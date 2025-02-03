4cb299fef6b0ad44bc894e5bdd595709240d6fa4
BekirLee <nihadbekirli8@gmail.com>
2025-02-03 23:47:08 +0300

    some changes and updates for subscribe,share an dother btns

--- src/components/CurrentVideo.jsx
+++ src/components/CurrentVideo.jsx
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom';
-import { AiOutlineLike, AiOutlineDislike } from "react-icons/ai";
+import { AiOutlineLike, AiFillDislike, AiOutlineDislike } from "react-icons/ai";
 import { PiShareFat } from "react-icons/pi";
 import { LiaDownloadSolid } from "react-icons/lia";
 import { IoCut } from "react-icons/io5";
 import { HiOutlineDotsHorizontal } from "react-icons/hi";
+import { BiSolidLike } from "react-icons/bi";
 // import 'dotenv/config';
 
 
@@ -21,6 +22,9 @@ function CurrentVideo() {
     const [likes, setLikes] = useState(null);
     const [channelInfos, setChannelInfos] = useState(null);
     const [searchParams] = useSearchParams();
+    const [liked, setLiked] = useState(false);
+    const [disliked, setDisliked] = useState(false);
+    const [subscribed, setSubscribed] = useState(false);
     const videoId = searchParams.get('id'); // Query parametresinden videoId'yi al
     const apikey = import.meta.env.VITE_API_KEY;
     const currentVideoFetch = async () => {
@@ -72,6 +76,41 @@ function CurrentVideo() {
         hiddenBox.classList.toggle('block')
     }
 
+    const likeClick = () => {
+        if (liked == 0) {
+            setLiked(true)
+        }
+        else if (liked == 1) {
+            setLiked(false)
+        }
+    }
+
+    const dislikeClick = () => {
+        if (disliked == 0) {
+            setDisliked(true)
+        }
+        else if (disliked == 1) {
+            setDisliked(false)
+        }
+    }
+
+    const subscriber = () => {
+        let subBtn = document.querySelector('.subBtn');
+        let innerSubBtn = document.querySelector('.innerSubBtn');
+        let btnSubParent = document.querySelector('.btnSubParent');
+        // btnSubParent.style.
+        // subBtn.classList.toggle('hidden');
+        // innerSubBtn.classList.toggle('hidden');
+
+        if (subscribed == 0) {
+            setSubscribed(true)
+        }
+        else if (subscribed == 1) {
+            setSubscribed(false)
+        }
+    }
+
+
     return (
         <div className='p-24 pr-8 pl-0'>
 
@@ -88,10 +127,10 @@ function CurrentVideo() {
                     allowFullScreen
                 ></iframe >
             </div>
-            <div className=" text-white leading-8 mt-3 mb-3">
+            <div className=" text-white leading-8 mt-4 mb-3">
                 <h3 className='font-bold text-[21px]'>{videoData?.snippet?.title}</h3>
 
-                <div className="flex gap-2 items-center h-14 justify-between">
+                <div className="flex gap-2 items-center mt-3 h-14 justify-between">
                     <div className="flex gap-4">
 
                         <div className="w-11 h-11 object-cover">
@@ -109,15 +148,16 @@ function CurrentVideo() {
                             </div>
                         </div>
 
-                        <div className="btn">
-                            <button className='bg-white hover:bg-[#ffffffd8] rounded-full w-[60px] text-black font-bold pt-1 pb-1'>
-                                Join
-                            </button>
-                        </div>
-
-                        <div className="btn bg-grey hover:bg-[#ffffffd8] rounded-full w-[60px] text-black font-bold pt-1 pb-1">
-                            <button className>
-                                Subscribe
+                        <div className="btnSubParent hover:bg-[#ffffff8d] rounded-full text-black font-bold ">
+                            <button className='subBtn h-full'
+                                onClick={subscriber}>
+                                {
+                                    !subscribed ? <div className='bg-white p-1 rounded-full h-full'>Subscribe</div> : <div className="innerSubBtn w-[170px] h-full flex items-center p-1 rounded-full hover:bg-neutral-600 ">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 96 96" width="30" height="30" preserveAspectRatio="xMidYMid meet"><defs><clipPath id="__lottie_element_160"><rect width="96" height="96" x="0" y="0"></rect></clipPath></defs><g clip-path="url(#__lottie_element_160)"><g transform="matrix(1,0.000002481389628883335,-0.000002481389628883335,1,47.99993133544922,48)" opacity="1"><g opacity="1" transform="matrix(1,0,0,1,0,0)"></g><g opacity="1" transform="matrix(1,0,0,1,0,0)"><path fill="currentcolor" fill-opacity="1" d=" M-6.5,26.410999298095703 C-6.5,26.410999298095703 6.5,26.410999298095703 6.5,26.410999298095703 C6.5,29.923999786376953 3.575000047683716,32.79899978637695 0,32.79899978637695 C-3.575000047683716,32.79899978637695 -6.5,29.923999786376953 -6.5,26.410999298095703z M22.75,19.290000915527344 C22.75,19.290000915527344 16.25,13.28600025177002 16.25,13.28600025177002 C16.25,13.28600025177002 16.25,-4.184000015258789 16.25,-4.184000015258789 C16.25,-12.071999549865723 12.381999969482422,-18.107999801635742 6.077000141143799,-20.47100067138672 C1.9819999933242798,-22.163999557495117 -2.502000093460083,-22.06800079345703 -6.4019999504089355,-20.375 C-12.51200008392334,-17.947999954223633 -16.25,-11.944000244140625 -16.25,-4.184000015258789 C-16.25,-4.184000015258789 -16.25,13.28600025177002 -16.25,13.28600025177002 C-16.25,13.28600025177002 -22.75,19.290000915527344 -22.75,19.290000915527344 C-22.75,19.290000915527344 -22.75,20.02400016784668 -22.75,20.02400016784668 C-22.75,20.02400016784668 22.75,20.02400016784668 22.75,20.02400016784668 C22.75,20.02400016784668 22.75,19.290000915527344 22.75,19.290000915527344z M26,17.947999954223633 C26,17.947999954223633 26,23.218000411987305 26,23.218000411987305 C26,23.218000411987305 -26,23.218000411987305 -26,23.218000411987305 C-26,23.218000411987305 -26,17.947999954223633 -26,17.947999954223633 C-26,17.947999954223633 -19.5,11.944000244140625 -19.5,11.944000244140625 C-19.5,11.944000244140625 -19.5,-4.502999782562256 -19.5,-4.502999782562256 C-19.5,-13.829000473022461 -14.430000305175781,-21.173999786376953 -6.5,-23.60099983215332 C-6.5,-23.60099983215332 -6.5,-24.81399917602539 -6.5,-24.81399917602539 C-6.5,-29.349000930786133 -1.6579999923706055,-32.79800033569336 3.2170000076293945,-30.434999465942383 C5.329999923706055,-29.413000106811523 6.5,-27.145000457763672 6.5,-24.81399917602539 C6.5,-24.81399917602539 6.5,-23.569000244140625 6.5,-23.569000244140625 C14.430000305175781,-21.173999786376953 19.5,-13.795999526977539 19.5,-4.4710001945495605 C19.5,-4.4710001945495605 19.5,11.97599983215332 19.5,11.97599983215332 C19.5,11.97599983215332 26,17.947999954223633 26,17.947999954223633z"></path><path fill='#fff' fill-opacity="1" d=" M-6.5,26.410999298095703 C-6.5,26.410999298095703 6.5,26.410999298095703 6.5,26.410999298095703 C6.5,29.923999786376953 3.575000047683716,32.79899978637695 0,32.79899978637695 C-3.575000047683716,32.79899978637695 -6.5,29.923999786376953 -6.5,26.410999298095703z M22.75,19.290000915527344 C22.75,19.290000915527344 16.25,13.28600025177002 16.25,13.28600025177002 C16.25,13.28600025177002 16.25,-4.184000015258789 16.25,-4.184000015258789 C16.25,-12.071999549865723 12.381999969482422,-18.107999801635742 6.077000141143799,-20.47100067138672 C1.9819999933242798,-22.163999557495117 -2.502000093460083,-22.06800079345703 -6.4019999504089355,-20.375 C-12.51200008392334,-17.947999954223633 -16.25,-11.944000244140625 -16.25,-4.184000015258789 C-16.25,-4.184000015258789 -16.25,13.28600025177002 -16.25,13.28600025177002 C-16.25,13.28600025177002 -22.75,19.290000915527344 -22.75,19.290000915527344 C-22.75,19.290000915527344 -22.75,20.02400016784668 -22.75,20.02400016784668 C-22.75,20.02400016784668 22.75,20.02400016784668 22.75,20.02400016784668 C22.75,20.02400016784668 22.75,19.290000915527344 22.75,19.290000915527344z M26,17.947999954223633 C26,17.947999954223633 26,23.218000411987305 26,23.218000411987305 C26,23.218000411987305 -26,23.218000411987305 -26,23.218000411987305 C-26,23.218000411987305 -26,17.947999954223633 -26,17.947999954223633 C-26,17.947999954223633 -19.5,11.944000244140625 -19.5,11.944000244140625 C-19.5,11.944000244140625 -19.5,-4.502999782562256 -19.5,-4.502999782562256 C-19.5,-13.829000473022461 -14.430000305175781,-21.173999786376953 -6.5,-23.60099983215332 C-6.5,-23.60099983215332 -6.5,-24.81399917602539 -6.5,-24.81399917602539 C-6.5,-29.349000930786133 -1.6579999923706055,-32.79800033569336 3.2170000076293945,-30.434999465942383 C5.329999923706055,-29.413000106811523 6.5,-27.145000457763672 6.5,-24.81399917602539 C6.5,-24.81399917602539 6.5,-23.569000244140625 6.5,-23.569000244140625 C14.430000305175781,-21.173999786376953 19.5,-13.795999526977539 19.5,-4.4710001945495605 C19.5,-4.4710001945495605 19.5,11.97599983215332 19.5,11.97599983215332 C19.5,11.97599983215332 26,17.947999954223633 26,17.947999954223633z"></path></g></g></g></svg>
+                                        <span className='text-white'>Subscribed!!!!!</span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 0 24 24" width="30" focusable="false" aria-hidden="true" fill='#fff' ><path d="m18 9.28-6.35 6.35-6.37-6.35.72-.71 5.64 5.65 5.65-5.65z"></path></svg>
+                                    </div>
+                                }
                             </button>
                         </div>
                     </div>
@@ -125,44 +165,53 @@ function CurrentVideo() {
                     <div className="flex gap-3">
 
                         <div className="btns bg-[#ffffff35] rounded-full w-[140px] flex justify-around h-[40px]">
-                            <button className='like hover:bg-[#ffffff2a] w-full pl-5 pr-2 rounded-l-full flex items-center'>
-                                <AiOutlineLike style={{ width: "25px", height: "25px" }} />
+                            <button className='like hover:bg-[#fff3] w-full pl-5 pr-2 rounded-l-full flex items-center'
+                                onClick={likeClick}>
+                                {
+                                    liked ? <AiOutlineLike style={{ width: "25px", height: "25px" }} /> : <BiSolidLike style={{ width: "25px", height: "25px" }} />
+                                }
+
                                 {formatViewCount(likes)}
                             </button>
                             <div className="h-[30px] w-[1px] bg-[#ffffff77] mt-1.5"></div>
-                            <button className='dislike hover:bg-[#ffffff2a] w-full pl-5 rounded-r-full'>
-                                <AiOutlineDislike style={{ width: "25px", height: "25px" }} />
+                            <button className='dislike hover:bg-[#fff3] w-full pl-5 rounded-r-full'
+                                onClick={dislikeClick}>
+                                {
+                                    disliked ? <AiOutlineDislike style={{ width: "25px", height: "25px" }} /> : <AiFillDislike style={{ width: "25px", height: "25px" }} />
+                                }
+
+
                             </button>
                         </div>
 
-                        <div className="share rounded-full bg-[#ffffff35] hover:bg-[#ffffff4d] pl-4 pr-4">
+                        <div className="share rounded-full bg-[#ffffff35] hover:bg-[#fff3] pl-4 pr-4">
                             <button className='flex items-center h-full'>
                                 <PiShareFat style={{ color: "white", width: "25px", height: "25px" }} />
                                 Share
                             </button>
                         </div>
 
-                        <div className="download rounded-full  bg-[#ffffff35] hover:bg-[#ffffff4d] pl-4 pr-4">
+                        <div className="download rounded-full  bg-[#ffffff35] hover:bg-[#fff3] pl-4 pr-4">
                             <button className='flex items-center h-full'>
                                 <LiaDownloadSolid />
                                 Download
                             </button>
                         </div>
 
-                        <div className="dots flex items-center justify-center bg-[#ffffff35] hover:bg-[#ffffff4d] rounded-full w-9 h-9 relative"
+                        <div className="dots flex items-center justify-center bg-[#ffffff35] hover:bg-[#fff3] rounded-full w-9 h-9 relative"
                             onClick={() => dotFunciton()}>
                             <HiOutlineDotsHorizontal />
                             <div className="absolute w-[120px] bottom-[40px] left-[10px] hiddenBox hidden bg-[#212121] z-[70] rounded-xl pt-2 pb-2">
                                 <ul className='h-full'>
-                                    <li className='flex justify-center gap-4 pl-2 pr-6 items-center hover:bg-[#ffffff4d] h-9 '>
+                                    <li className='flex justify-center gap-4 pl-2 pr-6 items-center hover:bg-[#fff3] h-9 '>
                                         <IoCut style={{ width: "24px", height: "24px" }} />
                                         Clip
                                     </li>
-                                    <li className='flex justify-center gap-4 pl-2 pr-6  items-center hover:bg-[#ffffff4d] h-9'>
+                                    <li className='flex justify-center gap-4 pl-2 pr-6  items-center hover:bg-[#fff3] h-9'>
                                         <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true" fill='currentcolor' ><path d="M18 4v15.06l-5.42-3.87-.58-.42-.58.42L6 19.06V4h12m1-1H5v18l7-5 7 5V3z"></path></svg>
                                         Save
                                     </li>
-                                    <li className='flex justify-center gap-4 pl-2 pr-6  items-center hover:bg-[#ffffff4d] h-9'>
+                                    <li className='flex justify-center gap-4 pl-2 pr-6  items-center hover:bg-[#fff3] h-9'>
                                         <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true" fill='currentcolor'><path d="m13.18 4 .24 1.2.16.8H19v7h-5.18l-.24-1.2-.16-.8H6V4h7.18M14 3H5v18h1v-9h6.6l.4 2h7V5h-5.6L14 3z"></path></svg>
                                         Report
                                     </li>
